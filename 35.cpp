69e82e88a299b700bfb80ea1b138534119f5c1ad
Prachi04Sharma <122223354+Prachi04Sharma@users.noreply.github.com>
2024-07-16 14:54:46 +0530

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+int main(){
+    //max heap
+    priority_queue<int> max;
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> mini;
+    max.push(1);
+    max.push(2);
+    max.push(3);
+    max.push(4);
+    cout<<"sign "<<max.size()<<endl;
+    int n = max.size();
+    for(int i = 0; i<max.size();i++ ){
+        cout<<max.top()<<" ";
+        max.top();
+    } 
+    cout << endl;
+    mini.push(5);
+    mini.push(1);
+    mini.push(0);
+    mini.push(4);
+    mini.push(3);
+    int m=mini.size();
+    for(int i =0; i<m; i++){
+        cout<<mini.top()<<" ";
+        mini.pop();
+    }
+    cout<<endl;
+
+}
