159a417ee8fe6eed6eb440acd948190b3318b2ed
Prachi04Sharma <122223354+Prachi04Sharma@users.noreply.github.com>
2024-07-29 18:46:09 +0530

    Add files via upload

---
+++ 127Coding_ninjas.cpp
@@ -0,0 +1,38 @@
+//Coding Ninjas : Best time to buy and sell stocks
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumProfit(vector<int> &prices) {
+    int n = prices.size();
+    int profit = 0;
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > profit) {
+            profit = prices[i] - minPrice;
+        }
+    }
+
+    return profit;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of days: ";
+    cin >> n;
+
+    vector<int> prices(n);
+    cout << "Enter the stock prices: ";
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    int maxProfit = maximumProfit(prices);
+    cout << "The maximum profit is: " << maxProfit << endl;
+
+    return 0;
+}
